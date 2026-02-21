a4c942a763c170f63f06e5b1428ec449b7254afd
bruno.albuquerque <brunoalbuquerquer1@gmail.com>
2026-02-21 12:19:48 -0400

    feat #11: Corrigindo a conexão com o back para requisição de dados

--- src/core/services/api.ts
+++ src/core/services/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
 })
 
 api.interceptors.request.use((config) => {
